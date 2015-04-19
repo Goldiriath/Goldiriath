e047debb09cc38ee10375964853c5c490420a71e
derpfacedikr <dirkjansomsen@hotmail.com>
2015-04-19 13:11:24 +0200

    Adding items

--- src/me/dirkjan/goldiriath/Items.java
+++ src/me/dirkjan/goldiriath/Items.java
@@ -7,7 +7,18 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public enum Items {
 
-    WOODEN_SWORD(Material.WOOD_SWORD, "battered wooden sword", 5, "absolute shit");
+    BAD_SWORD_WOOD(Material.WOOD_SWORD, "battered wooden sword", 1, "absolute shit"),
+    NORMAL_SWORD_WOOD(Material.WOOD_SWORD, "normal wooden sword", 1, "not special"),
+    CRAFTED_SWORD_WOOD(Material.WOOD_SWORD, "crafted wooden sword", 1, "made by a player"),
+    GOOD_SWORD_WOOD(Material.WOOD_SWORD, "rare wooden sword", 1, "an amazing wood sword"),
+    AMAZING_SWORD_WOOD(Material.WOOD_SWORD, "legendary wooden sword", 1, "the best wooden sword"),
+    BAD_CHESTPLATE_LEATHER(Material.LEATHER_CHESTPLATE, "used leather chestplate", 1, "absolute shit"),
+    NORMAL_CHESTPLATE_LEATHER(Material.LEATHER_CHESTPLATE, "normal leather chestplate", 1, "not special"),
+    CRAFTED_CHESTPLATE_LEATHER(Material.LEATHER_CHESTPLATE, "crafted leather chestplate", 1, "made by a player"),
+    GOOD_CHESTPLATE_LEATHER(Material.LEATHER_CHESTPLATE, "rare leather chestplate", 1, "an amazing leather chestplate"),
+    AMAZING_CHESTPLATE_LEATHER(Material.LEATHER_CHESTPLATE, "legandary leather chestplate", 1, "the best leather chestplate");
+    
+    
 
     private final ItemStack stack;
 
